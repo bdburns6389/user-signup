39a12829be1bbb82d32baf8486acbfb43d7a2aab
Brian Burns <bdburns6389@gmail.com>
2017-11-22 21:30:13 -0600

    needs the right if statements.

--- main.py
+++ main.py
@@ -17,22 +17,22 @@ def welcome():
     password= request.form['password']
     verify_password = request.form['verify_password']
     email = request.form['email']
-    if len(username)< 1:
+    #if any are wrong, template will pass all of them
+    if len(username) < 3 or len(username) > 20:
         return render_template('index.html', user_error=user_error, username=username)
 
-    if len(password) < 1:
+    if len(password) < 3 or len(password) > 20:
         return render_template('index.html', pass_error=pass_error, username=username)
     
     if password != verify_password:
         return render_template('index.html', verify_error=match_error)
     
-    if "." not in email or "@" not in email:
-        return render_template('index.html', email_error=email_error)
-
+    if len(email) > 1:
+        #works, but only called if everything else is valid.
+        if "." not in email or "@" not in email or len(email) < 8 or len(email) > 28:
+            return render_template('index.html', email_error=email_error)
     
     return render_template('welcome.html', username=username)
-
-    
 
 
 @app.route("/")
