268fc15d7aea9a8e89a13316ccaa2589403f4f7e
Brian Burns <bdburns6389@gmail.com>
2017-11-24 20:02:11 -0600

    syntax error on line 35

--- main.py
+++ main.py
@@ -6,10 +6,7 @@ import cgi
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-user_error = ""
-pass_error = ""
-match_error = ""
-email_error = ""
+
 
 @app.route("/input", methods=['POST'])
 def welcome():
@@ -17,7 +14,11 @@ def welcome():
     password= request.form['password']
     verify_password = request.form['verify_password']
     email = request.form['email']
-    #if any are wrong, template will pass all of them
+    user_error = ""
+    pass_error = ""
+    match_error = ""
+    email_error = ""
+    
     if len(username) < 3 or len(username) > 20:
         user_error = "That's not a valid username."
 
@@ -28,12 +29,13 @@ def welcome():
         match_error = "Passwords don't match."
     
     if len(email) > 1:
-        #works, but only called if everything else is valid.
         if "." not in email or "@" not in email or len(email) < 8 or len(email) > 28:
             email_error = "That's not a valid email."
     
-    return render_template('welcome.html', username=username)
-
+    if user_error = "" and pass_error = "" and match_error = "" and email_error = "":
+        return render_template('welcome.html', username=username)
+    else: 
+        return render_template('index.html', username=username, email=email, user_error=user_error, pass_error=pass_error, match_error=match_error, email_error=email_error)
 
 @app.route("/")
 def index():
